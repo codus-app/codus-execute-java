bb4613a515e68ccdceae40ab2a8a43372938f23f
Luke Taylor <luke@deentaylor.com>
2017-12-19 21:33:16 -0500

    Beginning of Tester class

---
+++ Tester.java
@@ -0,0 +1,22 @@
+// File IO
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+
+public class Tester {
+  // Returns a String with the contents of a file
+  static String readFile (String path, Charset encoding) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    return new String(bytes, encoding);
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.println("tests.json --------------------------------------------------------------\n");
+    System.out.println(
+      Tester.readFile("./test/tests.json", StandardCharsets.UTF_8)
+    );
+    System.out.println("-------------------------------------------------------------------------");
+  }
+}
