d7c4dd66c414c690665287dde6f6a4221f06fe94
Luke Taylor <luke@deentaylor.com>
2017-12-20 23:50:17 -0500

    Add TestCase.getJavaObject

--- TestCase.java
+++ TestCase.java
@@ -1,5 +1,6 @@
-import com.eclipsesource.json.JsonObject;
+// JSON
 import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
 
@@ -17,5 +18,28 @@ class TestCase {
 
   public String toString() {
     return "TestCase " + this.parameters.toString() + " -> " + this.expectedResult.toString();
+  }
+
+  // Return the plain-java representation of a JsonValue
+  public static Object getJavaObject(JsonValue value) {
+    // Basics
+    if (value.isString()) return value.asString();
+    if (value.isBoolean()) return value.asBoolean();
+    if (value.isNull()) return null;
+    // Numbers could be int or double
+    if (value.isNumber()) {
+      if (value.asDouble() == (int) value.asDouble()) return value.asInt();
+      else return value.asDouble();
+    }
+    // Can't deserialize JSON object
+    if (value.isObject()) return value.asObject();
+    // Arrays require recursion
+    if (value.isArray()) {
+      JsonArray arr = value.asArray();
+      Object[] out = new Object[arr.size()];
+      for (int i = 0; i < arr.size(); i++) out[i] = getJavaObject(arr.get(i));
+      return out;
+    }
+    return null;
   }
 }
