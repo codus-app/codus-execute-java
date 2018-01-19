e7a34b20da135c52cbdb798974dec59dbf8acf09
Luke Taylor <luke@deentaylor.com>
2018-01-19 11:13:36 -0500

    Attempt 1 at automatically building image

--- app/index.js
+++ app/index.js
@@ -4,3 +4,17 @@ const Docker = require('dockerode');
 
 // Initialize Docker
 const docker = new Docker();
+
+// Make sure that the image is built
+docker.listImages().then(images => {
+  // Check for the presence of an image tagged codus-execute-java:latest
+  if (!images.filter(i => i.RepoTags.includes('codus-execute-java:latest')).length) {
+    // If none is present, build the image
+    docker.buildImage({
+      context: path.join(__dirname, '..'),
+      src: ['Dockerfile'],
+    }, {
+      t: 'codus-execute-java'
+    }).then(n => n.pipe(process.stdout));
+  }
+});
