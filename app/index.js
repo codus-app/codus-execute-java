45a61e87452b3f524708dcc8a7194cbe79900f01
Luke Taylor <luke@deentaylor.com>
2018-01-19 20:08:18 -0500

    Don't export

--- app/index.js
+++ app/index.js
@@ -6,16 +6,18 @@ const Docker = require('dockerode');
 const docker = new Docker();
 
 // Make sure that the image is built
-module.exports.preflight = () => docker.listImages().then(images => {
-  // Check for the presence of an image tagged codus-execute-java:latest
-  if (!images.filter(i => i.RepoTags.includes('codus-execute-java:latest')).length) {
-    // If none is present, build the image
-    // FIXME: waiting on https://github.com/apocas/dockerode/issues/432
-    docker.buildImage({
-      context: path.join(__dirname, '..'),
-      src: ['Dockerfile'],
-    }, {
-      t: 'codus-execute-java'
-    }).then(n => n.pipe(process.stdout));
-  }
-});
+function preflight() {
+  docker.listImages().then(images => {
+    // Check for the presence of an image tagged codus-execute-java:latest
+    if (!images.filter(i => i.RepoTags.includes('codus-execute-java:latest')).length) {
+      // If none is present, build the image
+      // FIXME: waiting on https://github.com/apocas/dockerode/issues/432
+      docker.buildImage({
+        context: path.join(__dirname, '..'),
+        src: ['Dockerfile'],
+      }, {
+        t: 'codus-execute-java'
+      }).then(n => n.pipe(process.stdout));
+    }
+  });
+}
